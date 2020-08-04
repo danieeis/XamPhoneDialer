9089ceb749ae0c5250599f1b37cf6a529f240f13
Daniel Tovar <danieldaniyyelda@gmail.com>
2020-08-04 17:44:22 -0400

    Add project files.

---
+++ Phoneword/Phoneword/MainPage.cs
@@ -0,0 +1,85 @@
+﻿namespace Phoneword
+{
+    using System;
+    using Xamarin.Essentials;
+    using Xamarin.Forms;
+    public class MainPage : ContentPage
+    {
+        Entry phoneNumberText;
+        Button translateButton;
+        Button callButton;
+        string translateNumber;
+        public MainPage()
+        {
+            this.Padding = new Thickness(20);
+            StackLayout panel = new StackLayout
+            {
+                Spacing = 15
+            };
+            panel.Children.Add(new Label()
+            {
+                Text = "Enter a Phoneword:",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+            });
+            panel.Children.Add(phoneNumberText = new Entry()
+            {
+                Text = "1-855-XAMARIN"
+            });
+            panel.Children.Add(translateButton = new Button()
+            {
+                Text = "Translate"
+            });
+            panel.Children.Add(callButton = new Button()
+            {
+                Text = "Call",
+                IsEnabled = false
+            });
+
+            translateButton.Clicked += OnTranslate;
+            callButton.Clicked += OnCall;
+            this.Content = panel;
+
+        }
+        private async void OnCall(object sender, EventArgs e)
+        {
+            if(await DisplayAlert(
+                "Dial a Number", 
+                $"Would you like to call {translateNumber}?", 
+                "Yes", 
+                "No"))
+            {
+                try
+                {
+                    PhoneDialer.Open(translateNumber);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+                }
+                catch (FeatureNotSupportedException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone dialing not supported.", "OK");
+                }
+                catch (Exception)
+                {
+                    await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+                }
+            }
+        }
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            var result = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+            if (result != null)
+            {
+                translateNumber = result;
+                callButton.IsEnabled = true;
+                callButton.Text = $"Call {translateNumber}";
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
+    }
+}
